2e0c21cedf020968c28c7b542148bd4bbd15ac84
Pranav Kulkarni <pranavkulkarni195@gmail.com>
2021-01-08 16:26:33 +0530

    fixed

---
+++ three.sh
@@ -0,0 +1,13 @@
+
+sudo aptitude install python3-pip  
+ figlet \
+  zsh\
+   flameshot \
+    cmatrix \
+     terminator \
+      nautilus \
+       gnome-themes-extra \
+        sl \
+         kdeconnect \
+          python3 \
+           powerline
